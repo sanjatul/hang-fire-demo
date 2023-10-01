2ae80077abce89f0bebe961ef02b391224f81669
SanjatulHasanSiam <sanjatulhasansiam.iit@gmail.com>
2023-10-01 13:23:37 +0600

    Add project files.

---
+++ hang-fire/Program.cs
@@ -0,0 +1,33 @@
+using Hangfire;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHangfire(x =>
+{
+    x.UseSqlServerStorage(builder.Configuration.GetConnectionString("DBConnection"));
+});
+builder.Services.AddHangfireServer();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HangfireApplication v1"));
+
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.UseHangfireDashboard();
+app.MapControllers();
+
+app.Run();
